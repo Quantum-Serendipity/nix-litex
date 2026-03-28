79b5d137fd5b4cf4b625050fdef76ec6e89056cf
colin-rushton-qss <colin.rushton@quantumserendipitysoftware.com>
2026-03-28 14:29:49 -0400

    Add branch ref for fork fetchGit
    GitHub upload-pack won't serve arbitrary commits by hash; need
    explicit ref to the branch so nix can fetch from the fork.

--- pkgs/litex/default.nix
+++ pkgs/litex/default.nix
@@ -34,6 +34,7 @@ buildPythonPackage rec {
 
   src = builtins.fetchGit {
     url = "https://github.com/${pkgMeta.github_user}/${pkgMeta.github_repo}";
+    ref = "refs/heads/${pkgMeta.git_branch}";
     rev = pkgMeta.git_revision;
   };
 
